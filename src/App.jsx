d3548ee635dca830dd190de8487c284b7302254c
Adrien <Av.adrien.villemin@gmail.com>
2022-11-24 11:55:56 +0100

    mise a jour

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Singleproduct from "./pages/Singleproduct";
+import Error from "./pages/Error";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/about" element={<About />} />
+        <Route path="products/:productId" element={<Singleproduct />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
